6895a14997a3660bd3b09b4165b5fee5d8c0de14
Zack Elias <zelias500@gmail.com>
2015-09-28 21:16:24 -0400

    implemented routing

--- models/index.js
+++ models/index.js
@@ -24,10 +24,77 @@ var User = require('./user')(twitterjsDB);
 User.hasMany(Tweet);
 Tweet.belongsTo(User);
 
+
+
 module.exports = {
     User: User,
-    Tweet: Tweet
+    Tweet: Tweet,
+    
+    getAllTweets: function() {
+      return Tweet.findAll({
+          include: [{
+            model: User,
+            attributes: ['name']
+          }]})
+        .then(function(tweets) {
+          return tweets.map(function(tweet){
+            return tweet.get({plain:true});
+          })
+        })
+    },
+
+    getUserTweets: function(username) {
+      return User.findOne({where: {name: username}})
+        .then(function(user) {
+         return user.getTweets();
+        })
+        .then(function(tweets) {
+            return tweets.map(function(tweet){
+            return tweet.get({plain:true});
+          })
+        })
+    },
+
+    getTweetById: function(username, id){
+      return User.findOne({ where: {name: username}, include: [{model: Tweet, where: { id: id}}]     })
+      .then(function(user) {
+        return user.get({plain:true});
+      }).then(function(user){
+        return user.Tweets[0];
+      })
+    },
+
+    createTweet: function(name, text){
+      return User.findOne({where: {name: name}}).then(function(user){
+          if (!user) {
+            return User.create({name:name}).then(function(newUser){
+              return JSON.stringify(newUser.id);
+            })
+          }
+          else return JSON.stringify(user.id);
+        }).then( function(id) {
+        Tweet.create({UserId: id, tweet:text})
+        .then(function(tweet){
+          console.log("ADDED A TWEET")
+        });
+      })
+    }
 };
+
+
+// User.findOne({where: {name: name}}).then(function(user){
+//   return JSON.stringify(user.id);
+// }).then( function(id) {
+//   Tweet.create({UserId: id, tweet:"asdfasdfasdf"}).then(function(tweet){
+//     console.log("ADDED A TWEET")
+//   });
+// }
+
+
+// Tweet.create({userid: '3', tweet: 'this is a new tweet'})
+// .then(function(msg) {
+//  console.log(msg);
+// });
 
 
 // User.findOne().then(function (user) {
@@ -44,7 +111,10 @@ module.exports = {
 // 	return user.getTweets();
 // })
 // .then(function(tweets) {
-// 	console.log(JSON.stringify(tweets));
+//    return tweets.map(function(tweet){
+//             return tweet.get({plain:true});
+//           })
+
 // })
 
 // FIND BY TWEET ID
@@ -54,14 +124,20 @@ module.exports = {
 // });
 
 
-// FIND ALL TWEETS WITH USER ID
+// FIND ALL TWEETS (ALONG WITH THEIR USERS)
 // Tweet.findAll({include: [User]})
 // .then(function(everything) {
 // 	console.log(JSON.stringify(everything));
 // })
 
-Tweet.create({userid: '3', tweet: 'this is a new tweet'})
-.then(function(msg) {
-	console.log(msg);
-});
+// Tweet.findAll({
+//           include: [{
+//             model: User,
+//             attributes: ['name']
+//           }]})
+//         .then(function(tweets) {
+//             console.log(JSON.stringify(tweets));
+//         })
+
+
 
--- routes/index.js
+++ routes/index.js
@@ -4,33 +4,72 @@ var router = express.Router()
 
 var tweetbank = require('../tweetbank')
 
+var tweetDatabase = require('../models/');
+
 module.exports = router
+
+console.log("what up")
 
 // home
 router.get('/', function(req, res, next) {
-  res.render('index', { tweets: tweetbank.list(), showForm:true })
+	tweetDatabase.getAllTweets().then(function(data){
+		var tweetList = data.map(function(tweet){
+			return {text: tweet.tweet, name: tweet.User.name, id: tweet.id};
+		})
+		res.render('index', {tweets:tweetList, showForm:true});
+	})
+  // res.render('index', {tweets: tweetDatabase.getAllTweets()});
+  // res.render('index', { tweets: tweetbank.list(), showForm:true })
 })
 
 // make a tweet
 router.post('/', function(req, res, next) {
   // res.status(201).json(tweetbank.add(req.body.name, req.body.tweet))
-  res.redirect('/')
+  // console.log(req.body.name, req.body.tweet);
+  tweetDatabase.createTweet(req.body.name, req.body.tweet)
+  .then(function(tweet){
+  	res.redirect('/')
+  })
 })
 
 // getting all tweets from user
 router.get('/users/:name', function(req, res, next) {
-  var tweets = tweetbank.find(req.params)
+  var username = req.params.name
+  tweetDatabase.getUserTweets(username).then(function(data){
+  	var tweetList = data.map(function(tweet){
+  	return 	{
+  		id: tweet.id,
+  		name: username,
+  		text: tweet.tweet}
+  	})
+  	res.render('index', { tweets: tweetList, showForm:true })
+  })
+
+  // var tweets = tweetbank.find(req.params)
   // res.json(tweets)
-  res.render('index', { tweets: tweets })
 })
 
 // get a single tweet
 router.get('/users/:name/tweets/:id', function(req, res, next) {
   req.params.id = Number(req.params.id)
-  var tweets = tweetbank.find(req.params)
-  res.render('index',{ tweets: tweets})
-})
+  id = req.params.id;
+  var username = req.params.name;
+  tweetDatabase.getTweetById(username, id).then(function(data){
+  	var tweetList = [{
+  	  		id: id,
+  	  		name: username,
+  	  		text: data.tweet
+  	  	}]
+  	console.log(data);
+  	console.log(tweetList);
 
+  	res.render('index',{ tweets: tweetList});
+  	
+  })
+  // var tweets = tweetbank.find(req.params)
+
+
+})
 
 
 
